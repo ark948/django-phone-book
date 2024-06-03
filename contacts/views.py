25696c1d0dae2f284478dc3e999ee65919e24ecb
ark948 <armankarimi948@gmail.com>
2024-06-03 17:42:47 +0330

    Added built-in thorttles

--- contacts/views.py
+++ contacts/views.py
@@ -2,9 +2,8 @@ from rest_framework import generics
 from contacts.models import Contact
 from contacts.serializers import ListContactSerializer, DetailContactSerializer
 from contacts.permissions import IsOwner
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, filters
+from rest_framework import filters
+from rest_framework.throttling import UserRateThrottle
 
 class ContactsList(generics.ListCreateAPIView):
     serializer_class = ListContactSerializer
@@ -22,6 +21,7 @@ class ContactsList(generics.ListCreateAPIView):
 class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = DetailContactSerializer
     permission_classes = (IsOwner, )
+    throttle_classes = [UserRateThrottle]
 
     def get_queryset(self):
         user = self.request.user
--- core/settings.py
+++ core/settings.py
@@ -69,6 +69,15 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": 'rest_framework.pagination.PageNumberPagination',
     "PAGE_SIZE": 5,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+
 }
 
 SPECTACULAR_SETTINGS = {
