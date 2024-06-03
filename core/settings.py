5708c4e900424e4433d482cb7631a3c10a83a90a
ark948 <armankarimi948@gmail.com>
2024-06-03 13:15:30 +0330

    Added simple filtering

--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": 'rest_framework.pagination.PageNumberPagination',
     "PAGE_SIZE": 5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 SPECTACULAR_SETTINGS = {
