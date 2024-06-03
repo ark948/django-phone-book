dec9d7e9875238b27cce81cbe1cc88aafdc44981
ark948 <armankarimi948@gmail.com>
2024-06-03 13:08:59 +0330

    Added pagination

--- core/settings.py
+++ core/settings.py
@@ -66,6 +66,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": 'rest_framework.pagination.PageNumberPagination',
+    "PAGE_SIZE": 5,
 }
 
 SPECTACULAR_SETTINGS = {
