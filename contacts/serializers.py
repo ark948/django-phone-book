dca6a61461ee7a357bd0fc22dc3aec33784067ef
ark948 <armankarimi948@gmail.com>
2024-06-03 13:21:33 +0330

    Added detail field to contacts list

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -3,10 +3,11 @@ from contacts.models import Contact
 
 class ListContactSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    details = serializers.HyperlinkedIdentityField(view_name='contacts:contact-detail')
 
     class Meta:
         model = Contact
-        fields = ["title", "full_name", "owner"]
+        fields = ["title", "full_name", "owner", 'details']
 
 class DetailContactSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
